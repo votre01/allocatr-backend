325a1e99160b736699e51f3f7aa9660629603af9
Trevor <trevormngwenya@gmail.com>
2025-02-12 08:08:20 +0200

    Add auth token verification

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { createUserController, getAllUsersController} from "../controllers/usersController"
+import { jwtCheck } from "../middleware/auth";
 
 const router = Router();
 
-router.post("/auth", createUserController);
+router.post("/auth", jwtCheck, createUserController);
 // router.get("/:id", getUserById);
 router.get("/", getAllUsersController);
 // router.patch("/users:id", updateUserById);
